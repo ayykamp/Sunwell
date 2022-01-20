586856fd54e3ae1a430e67f9ef5304c98bc17e73
AyyKamp <arda513@hotmail.de>
2022-01-20 13:44:48 +0100

    upgrade to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -5,24 +5,17 @@ const nodeExternals = require("webpack-node-externals");
 const PROD = process.env.NODE_ENV === "production";
 const PLATFORM = process.env.PLATFORM || "node";
 
-const TS_LOADER = {test: /\.ts$/, loaders: ["ts-loader"]};
-
 let plugins = [
 	new webpack.DefinePlugin({
 		"process.env.PLATFORM": JSON.stringify(PLATFORM),
 	}),
+	new webpack.ProvidePlugin({
+		process: "process/browser",
+	}),
+	new webpack.ProvidePlugin({
+		Buffer: ['buffer', 'Buffer'],
+	}),
 ];
-
-// if (PROD) {
-plugins.push(
-	new webpack.optimize.UglifyJsPlugin({
-		compress: true,
-		mangle: false,
-		beautify: true,
-	})
-);
-//   plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
-// }
 
 module.exports = [
 	{
@@ -34,18 +27,41 @@ module.exports = [
 		output: {
 			path: path.resolve(__dirname, "dist"),
 			filename: PROD ? "[name].min.js" : "[name].js",
-			library: "Sunwell",
-			//   libraryTarget: PLATFORM == 'web' ? 'var' : 'commonjs2'
-			libraryTarget: "umd",
-			umdNamedDefine: true,
+			library: {
+				type: 'module',
+			},
 		},
 		resolve: {
 			extensions: [".webpack.js", ".web.js", ".ts", ".js"],
+			fallback: {
+				assert: require.resolve("assert"),
+			},
 		},
-		externals: [nodeExternals()],
+		externals: PLATFORM === "node" ? [nodeExternals()]: [],
 		module: {
-			loaders: [TS_LOADER],
+			rules: [
+				{
+					enforce: 'post',
+					test: /linebreak[/\\]src[/\\]linebreaker.js/,
+					loader: 'transform-loader',
+					options: {
+						brfs: {}
+					}
+				},
+				{
+        	test: /\.ts?$/,
+        	use: 'ts-loader',
+        	exclude: /node_modules/,
+				},
+			],
 		},
+		optimization: {
+			minimize: PROD
+		},
+		mode: PROD ? 'production': 'development',
 		plugins,
+		experiments: {
+			outputModule: true,
+		},
 	},
 ];
